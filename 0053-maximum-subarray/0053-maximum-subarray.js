89347f8c5b00a1982fa35acf71f2ce763e402bc1
saintcodes <87248728+saintcodes@users.noreply.github.com>
2022-12-21 13:57:26 -0500

    Time: 90 ms (87.44%), Space: 50 MB (86.53%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -7,15 +7,16 @@
 var maxSubArray = function(nums) {
     let total = 0
     let max = nums[0]
-    for (i=0; i<nums.length; i++) {
+    let i = 0
+    while (i<nums.length){
         total += nums[i]
-        console.log(total, max)
         if (total > max) {
             max = total
         }
         if (total < 0) {
             total = 0
         }
+        i++
     }
     return max
 };
