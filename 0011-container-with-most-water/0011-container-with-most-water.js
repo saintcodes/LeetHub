f2366b6e188cbfc8fd05c3cd4b6a1a8fe57be70f
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-01-03 12:58:14 -0500

    Time: 4097 ms (5.01%), Space: 84 MB (5.02%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+
+var maxArea = function(height) {
+    let left = 0
+    let right = height.length-1
+    let max = 0
+
+    while (left < right) {
+        let lowest = Math.min(height[left], height[right])
+        let length = right-left
+        let current = length*lowest
+        console.log(height[left], height[right], length, lowest, current)
+        if (current > max) {
+            max = current
+        }
+        height[left] < height[right] ? left++ : right--
+    }
+return max
+}
