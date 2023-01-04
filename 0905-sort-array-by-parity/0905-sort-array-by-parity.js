3d874a5734d92e11bb5959b2ae1748fa9dfa5ad0
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-01-04 16:08:46 -0500

    Time: 121 ms (47.93%), Space: 44.7 MB (44.32%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.js
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.js
@@ -2,10 +2,12 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortArrayByParity = function(nums) {
-    let ans = []
-    for (let i = 0; i < nums.length; i++) {
-       nums[i] % 2 === 0 ? ans.unshift(nums[i]) : ans.push(nums[i])
-    }
-    return ans
-};
+// var sortArrayByParity = function(nums) {
+    
+var sortArrayByParity = A => A.filter(e=>e%2==0).concat(A.filter(e=>e%2==1))
+    // let ans = []
+    // for (let i = 0; i < nums.length; i++) {
+    //    nums[i] % 2 === 0 ? ans.unshift(nums[i]) : ans.push(nums[i])
+    // }
+    // return ans
+// };
