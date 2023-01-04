be9ed428ea68139fc6f57ee2e4be8bd4e6a63d92
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-01-04 16:14:35 -0500

    Time: 92 ms (65.39%), Space: 44.6 MB (63.33%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.js
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.js
@@ -2,14 +2,15 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-// var sortArrayByParity = function(nums) {
+
+var sortArrayByParity = function(nums) {
     
-//     let ans = []
-//     for (let i = 0; i < nums.length; i++) {
-//        nums[i] % 2 === 0 ? ans.unshift(nums[i]) : ans.push(nums[i])
-//     }
-//     return ans
-// };
+    let ans = []
+    for (let i = 0; i < nums.length; i++) {
+       nums[i] % 2 === 0 ? ans.unshift(nums[i]) : ans.push(nums[i])
+    }
+    return ans
+};
 
 
-var sortArrayByParity = A => A.filter(e=>e%2==0).concat(A.filter(e=>e%2==1))
+// var sortArrayByParity = A => A.filter(e=>e%2==0).concat(A.filter(e=>e%2==1))
