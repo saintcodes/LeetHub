cd9fb52316b3004af178019f277ea6300d5b71b2
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-01-04 17:05:45 -0500

    Time: 183 ms (42.67%), Space: 48.9 MB (35.45%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -6,7 +6,7 @@ var sortedSquares = function(nums) {
     // return nums.map((num) => Math.pow(nums[num],2))
     let ans = []
     for (let i =0; i <nums.length; i++) {
-        ans.push(Math.pow(nums[i], 2))
+        ans.push(nums[i]**2)
     }
     return ans.sort((a,b) => a-b)
 };
