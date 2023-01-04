cbcfd17e0134b99d4d76c445e13a43e2a1c31717
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-01-03 19:32:41 -0500

    Time: 58 ms (98.05%), Space: 43 MB (65.66%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -4,21 +4,18 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    let left = 0
-    let right = numbers.length-1
-    let indices = []
-    
-    while (left < right) {
-        if (numbers[left] + numbers[right] > target) {
-            right--
-        } else if (numbers[left] + numbers[right] === target) {
-            console.log(left, right)
-            indices.push(left+1)
-            indices.push(right+1)
-            break
-        } else {
-            left++
-        }
-        } 
-    return indices
+   let left = 0, right = numbers.length-1
+   let indices = []
+   while (left < right) {
+       if (numbers[left] + numbers[right] > target) {
+           right--
+       } else if (numbers[left] + numbers[right] === target) {
+           indices.push(left+1)
+           indices.push(right+1)
+           break
+       } else {
+        left++
+       }
+   }
+    return indices 
 };
