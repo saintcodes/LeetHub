d66c90ee06e9c3116c3a9a4bcd8d4b66884e2e49
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-01-10 18:45:35 -0500

    Time: 83 ms (62.34%), Space: 44.4 MB (25.26%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.js
+++ 0832-flipping-an-image/0832-flipping-an-image.js
@@ -12,9 +12,27 @@
 
 
 var flipAndInvertImage = function(image) {
-     for (let i = 0; i < image.length; i++) {
-        image[i].reverse()
-        image[i] = image[i].map((num) => num === 0 ? 1 : 0)
+//      for (let i = 0; i < image.length; i++) {
+//         image[i].reverse()
+//         image[i] = image[i].map((num) => num === 0 ? 1 : 0)
+//     }
+//     return image
+// };
+
+//other solution
+
+ let reversed = []
+    for (arr of image) {
+      reversed.push(arr.reverse())
     }
-    return image
-};
+    for (arr of reversed) {
+        for (let i = 0; i <arr.length; i++) {
+            if (arr[i] === 1) {
+                arr[i] = 0
+            } else {
+                arr[i] = 1
+            }
+        }
+    }
+    return reversed
+}
