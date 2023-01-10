5e2b93402c4c66cbd30b8b97de8840e4a00040d8
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-01-10 11:42:17 -0500

    Time: 71 ms (95.09%), Space: 45 MB (23.96%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -4,11 +4,13 @@
  */
 var removeDuplicates = function(nums) {
     let count = 1
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i-1] != nums[i]) {
+    let i = 1
+    while (i < nums.length) {
+        if (nums[i-1] !== nums[i]) {
             nums[count] = nums[i]
             count++
         }
+        i++
     }
     return count
 };
