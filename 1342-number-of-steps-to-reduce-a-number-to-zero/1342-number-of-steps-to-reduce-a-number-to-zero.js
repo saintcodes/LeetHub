40f9d65841fff98a1c6a3b21bd4fa8e36aee8821
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-01-17 19:53:50 -0500

    Time: 58 ms (93.10%), Space: 41.3 MB (99.14%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -2,16 +2,28 @@
  * @param {number} num
  * @return {number}
  */
-var numberOfSteps = function(num) {
-    count = 0
-    while (num > 0) {
-        if (num % 2 === 1) {
-            num = num - 1
-            count++
-        } else if (num % 2 === 0)  {
-            num = num / 2
-            count ++
-        }
+var numberOfSteps = function(num, count = 0) {
+    //basic solution
+    // count = 0
+    // while (num > 0) {
+    //     if (num % 2 === 1) {
+    //         num = num - 1
+    //         count++
+    //     } else if (num % 2 === 0)  {
+    //         num = num / 2
+    //         count ++
+    //     }
+    // }
+    // return count
+    
+    //recursive solution
+         if (num === 0) return 0
+    
+    if (num % 2 === 0) {
+        const count = numberOfSteps(num/2)
+        return 1 + count
+    } else {
+        const count = numberOfSteps(num-1)
+        return 1 + count
     }
-    return count
 };
