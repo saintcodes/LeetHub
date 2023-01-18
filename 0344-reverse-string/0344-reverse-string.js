99449aa46afe1bd1269781a71200e70c68d000d4
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-01-17 19:14:48 -0500

    Time: 87 ms (94.57%), Space: 48.3 MB (99.45%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -5,31 +5,33 @@
 var reverseString = function(s) {
     
     //basic solution
-    let left = 0
-    let right = s.length-1
-    let temp
+    // let left = 0
+    // let right = s.length-1
+    // let temp
     
-    while(left<right){
-        temp = s[left]
-        s[left] = s[right]
-        s[right] = temp
+//     while(left<right){
+//         temp = s[left]
+//         s[left] = s[right]
+//         s[right] = temp
         
-        left ++
-        right --
-    }
+//         left ++
+//         right --
+//     }
 
     //recursive solution
-    if (s.length === 1) {
-        return s
-    }
-    return s + s.length-1
+    //     if (s.length === 1) {
+    //     return s[0]
+    // }
+    // return s[s.length-1]+reverseString(s.slice(0,-1))
+    // // return [...reverseString(s.slice(1)), s[0]]
+
+    let l = 0
+    let r = s.length-1
     
-//     function sum(arr) {
-// 	if (arr.length === 1) {
-// 		return arr[0]
-// 	}
-// 	console.log(arr[0], sum(arr.slice(1)))
-// 	return arr[0] + sum(arr.slice(1))
-// }
+    while (l <= r) {
+        [s[l], s[r]] = [s[r], s[l]]
+        l+= 1
+        r-= 1
+    }
     
 };
