de436dfe4aa62e2ed858db82f055be12f170d8b6
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-02-07 15:48:58 -0500

    Time: 62 ms (75.54%), Space: 42.4 MB (10.31%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -9,8 +9,32 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+//first loop
+//   1 -> 2 -> 3 -> 4 -> 5 -> 6
+//   s
+//   f
+// //second loop
+//   1 -> 2 -> 3 -> 4 -> 5 -> 6
+//   s
+//        f
+// //third loop
+//   1 -> 2 -> 3 -> 4 -> 5 -> 6
+//        s
+//                  f
+// //fourth loop
+//   1 -> 2 -> 3 -> 4 -> 5 -> 6
+//             s
+//                            f
+// //fifth loop breaks beacuse now fast is null and returns index 4(linkedlist)
+//   1 -> 2 -> 3 -> 4 -> 5 -> 6
+//                  s
+//                                 f
+
 var middleNode = function(head) {
+    //sets both slow and fast pointer to head in the beginning
     let slow = head, fast = head
+    //while current index of fast and next index of fast is not null, execute the loop
+    //in this way, fast will always run twice as fast as slow, leaving slow as the middle index of the list
     while (fast !== null && fast.next !== null) {
         slow = slow.next
         fast = fast.next.next
