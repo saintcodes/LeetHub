d4f59d027dbb0edeab5bb19707cd87ab47e03065
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-02-07 13:14:05 -0500

    Time: 74 ms (65.90%), Space: 43.7 MB (89.20%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    let list = new ListNode(0)
+    let curr = list
+    
+    while (list1 !== null && list2 !== null) {
+        
+        if (list1.val < list2.val) {
+            curr.next = new ListNode(list1.val)
+            list1 = list1.next
+        } else {
+            curr.next = new ListNode(list2.val)
+            list2 = list2.next
+        }
+        curr = curr.next
+
+    }
+    
+    if (list1) {
+        curr.next = list1
+    } else {
+        curr.next = list2
+    }
+    return list.next
+
+};
