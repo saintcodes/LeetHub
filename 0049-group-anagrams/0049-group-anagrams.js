fa4475764f60e91b52829b536a3af4ea0aa28ee7
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-02-08 14:42:30 -0500

    Time: 131 ms (62.48%), Space: 54.6 MB (19.58%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let map = new Map
+    let results = []
+    
+    for (let i = 0; i < strs.length; i++) {
+        let sorted = strs[i].split('').sort().join('')
+        if (!map[sorted]) {
+            map[sorted] = []
+            map[sorted].push(strs[i])
+        } else {
+            map[sorted].push(strs[i])
+        }
+    }
+    return Object.values(map)
+};
