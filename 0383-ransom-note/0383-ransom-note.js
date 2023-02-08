2821ec4225c5896f767b87a2a56051c65a6af712
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-02-07 19:01:33 -0500

    Time: 97 ms (59.99%), Space: 44.4 MB (69.41%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+
+// ransomeNote, aa
+// magazine,    ab
+
+// magazine
+// a: 1
+// b: 1
+
+var canConstruct = function(ransomNote, magazine) {
+    let map = new Map
+    
+    for (el of magazine) {
+        map[el] = (map[el] || 0) +1
+    }
+    console.log(map)
+    
+    for (letter of ransomNote) {
+        if (letter in map) {
+            map[letter] -=1
+            if (map[letter]<0) return false
+        } else {
+            return false
+        }
+    }
+    return true
+};
