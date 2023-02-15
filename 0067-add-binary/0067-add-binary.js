be012c4a190674203ae672867acb868de51b49fa
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-02-15 16:48:07 -0500

    Time: 72 ms (53.74%), Space: 42.2 MB (86.81%) - LeetHub

--- 0067-add-binary/0067-add-binary.js
+++ 0067-add-binary/0067-add-binary.js
@@ -4,13 +4,12 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-   // let aBin = parseInt(BigInt(a),2)
-   // let bBin = parseInt(BigInt(b),2)
     const aBin = `0b${a}`
     const bBin = `0b${b}`
-   console.log(aBin, bBin)
+    // console.log(aBin, bBin)
     const sum = BigInt(aBin) + BigInt(bBin)
-    console.log(BigInt(aBin), BigInt(bBin))
-    console.log(sum)
+    // console.log(BigInt(aBin), BigInt(bBin))
+    // console.log(sum)
+    // console.log(sum.toString(2))
     return sum.toString(2)
 };
