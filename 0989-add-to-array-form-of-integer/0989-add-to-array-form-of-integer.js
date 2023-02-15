bf0e118b451d583addbb4569498f5250a26ab57e
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-02-15 16:22:29 -0500

    Time: 125 ms (46.04%), Space: 47.9 MB (58.36%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.js
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.js
@@ -6,6 +6,8 @@
 var addToArrayForm = function(num, k) {
     
     return `${BigInt(num.join("")) + BigInt(k)}`.split("")
+    
+    //below does not work due to big nums.
     // let converted = parseInt(num.join("")) + k
     // let text = converted.toString()
     // return text.split('')
