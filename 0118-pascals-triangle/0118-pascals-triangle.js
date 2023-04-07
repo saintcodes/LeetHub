5c12f8807690263b9c3c7ee68f20ecd212c8d2e7
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-04-08 00:16:19 +0800

    Time: 64 ms (47.94%), Space: 42.2 MB (68.31%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.js
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -5,18 +5,15 @@
 
 
 var generate = function(numRows) {
-    // if (numRows.length === 1) return [[1]]
-    // if (numRows.length === 2) return [[1], [1,1]]
-
     let result = []
     for (let i=0; i<numRows; i++) {
         let subArr = new Array(i+1)
         result.push(subArr)
-        for (let j=0; j<=i; j++) {
+        for (let j=0; j<result[i].length; j++) {
             if (j === 0 || j === i) {
                 result[i][j] = 1
             } else {
-                result[i][j] = result[i-1][j-1] + result[i-1][j]
+                result[i][j] = result[i-1][j] + result[i-1][j-1]
             }
         }
     }
