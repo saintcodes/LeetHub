3d958b102cf7b7d921d001271256a8c5a63f368c
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-04-08 01:00:31 +0800

    Time: 58 ms (65.81%), Space: 41.8 MB (68.04%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+
+//-
+//sadbutsad
+//
+//sad
+
+var strStr = function(haystack, needle) {
+    let index = 0
+   return haystack.indexOf(needle)
+
+};
