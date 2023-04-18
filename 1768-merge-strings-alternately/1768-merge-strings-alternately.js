bba40e1a36744a77a4d8172c328dd642f92bff80
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-04-19 00:14:41 +0800

    Time: 61 ms (49.90%), Space: 42.5 MB (42.32%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -4,22 +4,35 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-    let merged = ""
+    let merged = []
+    let m = word1.length, n = word2.length
     let i = 0
     let j = 0
     
-    while (i < word1.length && j < word2.length) {
-        merged+= word1[i]
-        i++
-        merged+= word2[j]
-        j++
-    }
-    if (i !== word1.length) {
-        merged += word1.slice(i)
-    }
-    if (j !== word2.length) {
-        merged += word2.slice(j)
+    while (i < m || j < n) {
+        if (i < m) {
+            merged += word1[i]
+            i +=1
+        }
+        if (j < n) {
+            merged+= word2[j]
+            j+=1
+        }
     }
     
     return merged
+//     while (i < word1.length && j < word2.length) {
+//         merged+= word1[i]
+//         i++
+//         merged+= word2[j]
+//         j++
+//     }
+//     if (i !== word1.length) {
+//         merged += word1.slice(i)
+//     }
+//     if (j !== word2.length) {
+//         merged += word2.slice(j)
+//     }
+    
+//     return merged
 };
