3661b92064fcd0c5181be3c8438b9bd29c02b55f
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-04-19 02:20:55 +0800

    Time: 93 ms (71.15%), Space: 51.2 MB (84.53%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -13,5 +13,5 @@ var rotate = function(nums, k) {
     // console.log(nums.slice(0, k+1))
     // console.log(nums.splice(k+1))
     if (k > nums.length) k = k%nums.length
-    nums.unshift(...nums.splice(nums.length-k, nums.length-1), ...nums.splice(0, nums.length-k))
+    nums.unshift(...nums.splice(nums.length-k, nums.length-1))
 };
