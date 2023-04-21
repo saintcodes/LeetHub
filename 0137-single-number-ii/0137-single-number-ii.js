14b0080eb865ae4178f0f66460504aabcf22571a
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-04-22 00:11:49 +0800

    Time: 69 ms (46.50%), Space: 45.3 MB (11.04%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    
+    let map = {}
+    for (num of nums) {
+        map[num] = (map[num] || 0) +1
+    }
+    for (el in map) {
+        if (map[el] === 1) {
+            return Number(el)
+        }
+    }
+};
