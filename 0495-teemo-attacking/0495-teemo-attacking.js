a7295a52b904dd2f4e301354b57ce4d78a929e14
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-05-04 14:31:36 -0400

    Time: 66 ms (61.70%), Space: 45.7 MB (51.06%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.js
+++ 0495-teemo-attacking/0495-teemo-attacking.js
@@ -4,28 +4,26 @@
  * @return {number}
  */
 var findPoisonedDuration = function(timeSeries, duration) {
-    // let seconds = 0
-    // for (let i=0; i <timeSeries.length; i++) {
-    //     if (duration >= timeSeries[i]) {
-    //         seconds += duration-timeSeries[i]
-    //         console.log(seconds, duration, timeSeries[i])
-    //     } else {
-    //         seconds += duration
-    //         console.log(seconds, duration, timeSeries[i])
-    //     }
-    // }
-    // return seconds
+    let seconds = 0
+    for (let i=0; i <timeSeries.length; i++) {
+        if ((timeSeries[i+1] - timeSeries[i]) < duration) {
+            seconds += timeSeries[i+1]-timeSeries[i]
+        } else {
+            seconds += duration
+        }
+    }
+    return seconds
     
 //     let answer = 0
 //     answer += Math.min(timeSeries.reduce((curr, acc) => curr + acc), duration)
 
 //     return answer
     
-    let total = 0
-    for (let i = 0; i < timeSeries.length; i++) {
-        let diff = timeSeries[i+1] - timeSeries[i]
-        if (diff < duration) total += diff
-        else total += duration
-    }
-    return total
+    // let total = 0
+    // for (let i = 0; i < timeSeries.length; i++) {
+    //     let diff = timeSeries[i+1] - timeSeries[i]
+    //     if (diff < duration) total += diff
+    //     else total += duration
+    // }
+    // return total
 };
