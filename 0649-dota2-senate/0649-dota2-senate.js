e4e46a3a44689416b9b33974fa61cfc1981d0f81
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-05-04 14:55:45 -0400

    Time: 80 ms (43.40%), Space: 47.1 MB (37.74%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.js
+++ 0649-dota2-senate/0649-dota2-senate.js
@@ -5,10 +5,14 @@
 var predictPartyVictory = function(senate) {
     const stack = [], arr = senate.split('');
 
-    console.log(arr)
-    for (let i = 0; i < arr.length; i++) {
-        if (!stack.length || stack.at(-1) === arr[i]) stack.push(arr[i]);
-        else arr.push(stack.pop());
+    for (let i = 0, count = 0; i < arr.length; i++) {
+        if (!stack.length || stack.at(-1) === arr[i]) {
+            stack.push(arr[i]);
+            count++
+        } else {
+            arr.push(stack.pop());
+            count++
+        }
     }
 
     return stack[0] === 'R' ? 'Radiant' : 'Dire';
