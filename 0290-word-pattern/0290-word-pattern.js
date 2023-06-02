7bbb97711206a2763233874eb7773d1cc23ee2b0
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-06-02 14:56:05 -0400

    Time: 53 ms (83.68%), Space: 42 MB (52.61%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.js
+++ 0290-word-pattern/0290-word-pattern.js
@@ -8,19 +8,22 @@ var wordPattern = function(pattern, s) {
     let letters = pattern.split('')
     let words = s.split(' ')
     
+    //checks if lengths of pattern and s is the same; if not, return false
     if (letters.length !== words.length) return false
     
+    //assigns key of each letter to a word if it doesn't exist in dict
+    //if the key exists and is not equal to the word, return false
     for (let i=0; i<words.length;i++) {
         if (!dict[letters[i]]) dict[letters[i]] = words[i]
         if (dict[letters[i]] !== words[i]) return false    
     }
     
+    //turns the array of [a,b,b,a] and ['dog', 'dog', 'dog', 'dog'] into a unique set
+    // => [a,b] and ['dog']
     let lettersSet = new Set(letters)
     let wordsSet = new Set(words)
     
-    console.log(lettersSet, wordsSet)
-
+    //evaluates size of each set and compares; if equal, return true, else false
     return wordsSet.size === lettersSet.size
  
-    console.log(dict)
 };
