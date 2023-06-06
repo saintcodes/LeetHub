cf443da684772fa6ae4f30cdae3cb400d0c3e713
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-06-06 11:20:36 -0400

    Time: 63 ms (34.97%), Space: 42.3 MB (18.18%) - LeetHub

---
+++ 1507-reformat-date/1507-reformat-date.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} date
+ * @return {string}
+ */
+var reformatDate = function(date) {
+
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    
+    let yyyy = date.slice(date.length-4, date.length)
+    let mm = parseInt(months.indexOf(date.slice(date.length-8, date.length-5)) + 1)
+    let dd = date.split(' ')[0]
+    let ddSliced = dd.slice(0, dd.length-2)
+ 
+    console.log(typeof mm, ddSliced)
+    if (mm < 10) mm = "0" + mm
+    if (ddSliced < 10) ddSliced = "0" + ddSliced
+    console.log(mm, ddSliced)
+    
+    return yyyy+"-"+mm+"-"+ddSliced
+};
