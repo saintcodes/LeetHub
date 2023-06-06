c2ecb09e3d0ce128b7480cb32087d96dddfeb9fa
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-06-05 23:37:38 -0400

    Time: 86 ms (6.50%), Space: 46 MB (6.21%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.js
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.js
@@ -3,23 +3,24 @@
  * @return {boolean}
  */
 var canMakeArithmeticProgression = function(arr) {
-    // arr.sort()
-    // console.log(arr)
-    // let diff = Math.abs(arr[1] - arr[0])
-    // for (let i = 1; i<arr.length-1; i++) {
-    //     let currDiff = Math.abs((arr[i+1]) - arr[i])
-    //     console.log(currDiff, diff)
-    //     if (currDiff !== diff) return false
-    // }
-    // return true
-    
-       if(arr.length < 3) return true
-    arr.sort((a,b)=>{return a-b})
+    arr.sort((a,b)=>a-b)
+    console.log(arr)
     let diff = arr[1] - arr[0]
-    for(let i=2;i<arr.length;i++){
-        if(arr[i] - arr[i-1] !== diff){
-           return false
-        }
+    for (let i = 2; i<arr.length; i++) {
+        let currDiff = arr[i] - arr[i-1]
+        // console.log(arr[i])
+        if (currDiff !== diff) return false
     }
     return true
+    
+    // arr.sort((a,b)=>{return a-b})
+    // let diff = arr[1] - arr[0]
+    // console.log(arr, diff)
+    // for(let i=2;i<arr.length;i++){
+    //     console.log(arr[i], arr[i-1], arr[i] - arr[i-1])
+    //     if(arr[i] - arr[i-1] !== diff){
+    //        return false
+    //     }
+    // }
+    // return true
 };
