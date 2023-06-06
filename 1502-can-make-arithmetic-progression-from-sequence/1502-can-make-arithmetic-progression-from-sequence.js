6f7d8b213f8dae948e346e3720fd19f77ec1f8cd
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-06-05 23:38:59 -0400

    Time: 89 ms (5.65%), Space: 46 MB (7.06%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.js
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 var canMakeArithmeticProgression = function(arr) {
-    arr.sort((a,b)=>a-b)
+    arr.sort((a,b) => a-b)
     console.log(arr)
     let diff = arr[1] - arr[0]
     for (let i = 2; i<arr.length; i++) {
