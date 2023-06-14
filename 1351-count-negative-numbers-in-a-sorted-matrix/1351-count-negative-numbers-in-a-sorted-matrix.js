05348f1c9ba8b1576d697d965e9365c3c331c8c4
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-06-14 10:36:33 -0400

    Time: 75 ms (12.08%), Space: 43.7 MB (87.17%) - LeetHub

---
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var countNegatives = function(grid) {
+    let count = 0
+    for (let arr of grid) {
+        for (num of arr) {
+            if (num < 0) count+=1
+        }
+    }
+    return count
+};
