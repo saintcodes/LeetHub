d11e3de99c4369ebcaacd487d3233af4436a73df
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-06-14 16:09:29 -0400

    Time: 4908 ms (5.08%), Space: 82.9 MB (5.08%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var equalPairs = function(grid) {
+    let rows = grid
+    let columns = []
+
+    for (let c = 0; c < grid[0].length; c++) {
+        let column = []
+        for (let r = 0; r < grid.length; r++) {
+            column.push(grid[r][c])
+        }
+        columns.push(column)
+    }
+    let same = 0
+    for (let i = 0; i < columns.length; i++) {
+        for (let j = 0; j <rows.length; j++) {
+            if (rows[j] + "" === columns[i] + "") same++
+        }
+    }
+    return same
+};
