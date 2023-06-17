5dd2e87a6853eaecef02d11f680893a85e8f1313
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-06-17 14:50:33 -0400

    Time: 76 ms (17.86%), Space: 45.3 MB (6.35%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var buddyStrings = function(s, goal) {
+    //base case
+    if (s.length !== goal.length) return false
+    if (s.length <2) return false
+    
+    //split 's' into array, checks if any differences, if so, push the index to diff array
+    let temp = s.split('')
+    let diff = []
+    for (let i=0; i<s.length; i++) {
+        if (s[i] !== goal[i]) diff.push(i)
+    }
+    console.log(diff)
+    
+    //if diff array length > 0, reassign and check if temp === goal
+    if (diff.length>0) {
+        let first = temp[diff[0]]
+        temp[diff[0]] = temp[diff[1]]
+        temp[diff[1]] = first
+        if (temp.join('') === goal) return true
+    }
+    
+    if (s === goal) {
+        let sMap = new Map()
+        for (let i =0; i<s.length; i++) {
+            sMap[s[i]] = (sMap[s[i]] || 0) +1
+        }
+        if (Object.keys(sMap).length == 1) {
+            return true
+        } else {
+            for (val in sMap) {
+                if (sMap[val] > 1) return true
+            }
+        }
+    }
+    return false
+};
