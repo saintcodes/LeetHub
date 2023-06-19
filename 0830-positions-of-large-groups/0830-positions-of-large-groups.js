353d09931dbfda4f32acef10780d97e52b3f5dbf
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-06-19 09:48:01 -0400

    Time: 77 ms (40.24%), Space: 45 MB (80.49%) - LeetHub

---
+++ 0830-positions-of-large-groups/0830-positions-of-large-groups.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number[][]}
+ */
+var largeGroupPositions = function(s) {
+    const res = [];
+    let start = 0;
+    
+    for (let i = 1; i <= s.length; i++) {
+        if (s[i] !== s[i - 1]) {
+            if (i - start >= 3) res.push([start, i - 1]);
+            start = i;
+        }
+    }
+    return res;
+
+};
