746110e28f82cd6dd20e301308346b4ea6b195cb
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-06-21 13:17:08 -0400

    Time: 453 ms (29.79%), Space: 46 MB (91.49%) - LeetHub

--- 0888-fair-candy-swap/0888-fair-candy-swap.js
+++ 0888-fair-candy-swap/0888-fair-candy-swap.js
@@ -7,11 +7,8 @@ var fairCandySwap = function(aliceSizes, bobSizes) {
 
     let aliceSum = aliceSizes.reduce((a,b) => a+b)
     let bobSum = bobSizes.reduce((a,b) => a+b)
-    let mean = (aliceSum+bobSum) / 2
-
-    
     let diff = (bobSum - aliceSum)/2
-    let res = []
+    
     for (let i=0; i<aliceSizes.length; i++) {
         let needFromBob = aliceSizes[i] + diff
         if (bobSizes.includes(needFromBob)) {
