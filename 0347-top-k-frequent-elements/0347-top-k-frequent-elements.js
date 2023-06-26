af82463c61e5437144e159d3845d228b653c71d5
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-06-26 17:05:40 -0400

    Time: 75 ms (66.11%), Space: 46.2 MB (32.37%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -13,14 +13,12 @@ var topKFrequent = function(nums, k) {
     }
     
     let arr = []
-    
     for (let [key, value] of Object.entries(freq)) {
         arr.push([key, value])
     }
     
     arr.sort((a, b) => b[1] - a[1])
     let res = []
-    console.log(arr)
 
     for (let i=0; i < k; i++) {
         res.push(arr[i][0])
