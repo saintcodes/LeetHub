0abc0e41b9ce6a2c4a20154ca35040e4178f1846
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-06-28 16:58:36 -0400

    Time: 65 ms (11.53%), Space: 41.4 MB (82.96%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.js
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.js
@@ -3,18 +3,6 @@
  * @return {number}
  */
 var countSegments = function(s) {
-//     let count = 0
-//     for (let i=0; i < s.length; i++) {
-//         if (i=0 || s[i-1] == ' ' && s[i] != "") {
-//             count+=1
-//         }
-//     }
-// //     let s1 = s.trim()
-    
-// //     if (s1 === '') return 0
-// //     console.log(s.split(" "))
-// //     return s.split(" ").length
-//     console.log(count)
         return s.split(' ').filter((e) => e !== '').length;
 
 };
