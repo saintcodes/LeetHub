748a2b6016def7aaacfaabf68df3931623f3a210
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-06-28 16:31:35 -0400

    Time: 74 ms (31.14%), Space: 44.4 MB (39.38%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.js
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -7,24 +7,12 @@ var findTheDifference = function(s, t) {
     let sArr = s.split('')
     let tArr = t.split('')
     let tEl = [...new Set(tArr)]
-    console.log(tArr)
-    console.log(tEl)
+
     
     let getFrequency = (arr, item) => {
         return arr.filter(x => x === item).length;
     };
     
-    for (let c of tEl) {
-        if ((getFrequency(sArr, c) != getFrequency(tArr, c))) return c;
-    }
-    
+    for (let c of tEl) if ((getFrequency(sArr, c) != getFrequency(tArr, c))) return c;  
 };
-
-//     let sArr = s.split("");
-//     let tArr = t.split("");
-//     let tElement = [...new Set(tArr)];
-//     for (const c of tElement) {
-//         if ((getFrequency(sArr, c) != getFrequency(tArr, c))) return c;
-//     }
-// };
 
