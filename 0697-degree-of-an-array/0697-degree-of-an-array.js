c2dd8317529abed891b421366956f4adf3d15f07
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-07-10 16:45:45 -0400

    Time: 284 ms (10.60%), Space: 51.8 MB (7.37%) - LeetHub

---
+++ 0697-degree-of-an-array/0697-degree-of-an-array.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findShortestSubArray = function(nums) {
+    let freq = {}
+    for (let i=0; i<nums.length; i++) {
+        freq[nums[i]] = (freq[nums[i]] || 0) +1
+    }
+    
+    let arr = [], checks = []
+    Object.entries(freq).forEach((entry) => arr.push(entry[1]))
+    let degree = Math.max(...arr)
+        
+    for (let [key, value] of Object.entries(freq)) {
+        if (value === degree) checks.push(+key)
+    }
+    let possibles = []
+    for (let i=0; i< checks.length; i++) {
+        let first = nums.indexOf(checks[i])
+        let last = nums.lastIndexOf(checks[i])
+        console.log(first, last)
+        console.log(possibles.push((last-first)+1))
+    }
+    
+    return Math.min(...possibles)
+   
+};
