1d11b5a8194afa067512d86f1ab75c39b682208c
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-07-10 14:26:19 -0400

    Time: 61 ms (84.02%), Space: 44.1 MB (86.54%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -4,11 +4,13 @@
  */
 var fizzBuzz = function(n) {
     let arr = []
-    for (let i = 1; i < n+1; i++) {
+    let i = 1
+    while (i <= n) {
         if (i % 3 === 0 && i % 5 === 0) arr.push('FizzBuzz')
         else if (i % 3 === 0) arr.push('Fizz')
         else if (i % 5 === 0) arr.push('Buzz')
         else arr.push(i.toString())
+        i++
     }        
     return arr
 };
