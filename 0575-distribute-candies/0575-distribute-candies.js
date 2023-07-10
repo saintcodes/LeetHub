2b381b425da94d52b9056474ce2b259f76ca5a81
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-07-10 15:15:16 -0400

    Time: 120 ms (44.52%), Space: 56.2 MB (45.89%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.js
+++ 0575-distribute-candies/0575-distribute-candies.js
@@ -3,6 +3,5 @@
  * @return {number}
  */
 var distributeCandies = function(candyType) {
-    let set = new Set(candyType)
-    return Math.min(set.size, candyType.length/2)
+    return Math.min(new Set(candyType).size, candyType.length/2)
 };
