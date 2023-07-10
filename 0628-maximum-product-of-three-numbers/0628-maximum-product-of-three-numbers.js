5d9114e60ff2e3c1287423d6b7aebbf9de9c9acf
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-07-10 15:23:53 -0400

    Time: 112 ms (69.97%), Space: 46.8 MB (55.91%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -3,8 +3,6 @@
  * @return {number}
  */
 var maximumProduct = function(nums) {
-    console.log(nums)
     nums.sort((a, b) => b-a)
-    console.log(nums)
     return Math.max(nums[0]*nums[1]*nums[2], nums[nums.length-1]*nums[nums.length-2]*nums[0])
 };
