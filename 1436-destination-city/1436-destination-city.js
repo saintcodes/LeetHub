18b23364b90c35ca3ad26e6f0b1888cf3687089d
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-07-17 10:45:24 -0400

    Time: 68 ms (49.65%), Space: 42.7 MB (94.44%) - LeetHub

--- 1436-destination-city/1436-destination-city.js
+++ 1436-destination-city/1436-destination-city.js
@@ -3,16 +3,27 @@
  * @return {string}
  */
 var destCity = function(paths) {
-    let inbound = {}
-    let outbound = {}
+//     let inbound = {}
+//     let outbound = {}
     
-    for (let i=0; i<paths.length; i++) {
-        inbound[paths[i][0]] = (inbound[paths[i][0]] || 0) +1
-    }
+//     for (let i=0; i<paths.length; i++) {
+//         inbound[paths[i][0]] = (inbound[paths[i][0]] || 0) +1
+//     }
     
-    for (let i=0; i<paths.length; i++) {
-        if (!(paths[i][1] in inbound)) outbound[paths[i][1]] = (outbound[paths[i][1]] || 0) +1
-    }
+//     for (let i=0; i<paths.length; i++) {
+//         if (!(paths[i][1] in inbound)) outbound[paths[i][1]] = (outbound[paths[i][1]] || 0) +1
+//     }
 
-    return Object.keys(outbound).toString()
+//     return Object.keys(outbound).toString()
+    let nextDest = paths[0][1];
+
+    
+    for (let i = 0; i < paths.length; i++) {
+       if (paths[i][0] == nextDest) {
+           nextDest = paths[i][1];
+           i = 0;
+       }
+    }
+    
+    return nextDest;
 };
