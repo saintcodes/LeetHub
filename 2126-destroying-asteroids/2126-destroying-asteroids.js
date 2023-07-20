2ea3196f6e5fa81766bddfea2aadbf0e41413ea1
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-07-20 14:33:20 -0400

    Time: 219 ms (17.02%), Space: 56.6 MB (76.60%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.js
+++ 2126-destroying-asteroids/2126-destroying-asteroids.js
@@ -5,10 +5,11 @@
  */
 var asteroidsDestroyed = function(mass, asteroids) {
     asteroids.sort((a,b) => a-b)
-    console.log(asteroids)
+
     for (let i=0; i<asteroids.length; i++) {
-        if (asteroids[i] > mass) return false
-        else mass+= asteroids[i]
+        let curr = asteroids[i]
+        if (curr > mass) return false
+        else mass+= curr
     }
     return true
 };
