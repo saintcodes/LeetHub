00c7ac75722eb8769f3a1ef7d32af98c926a344a
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-07-20 14:07:22 -0400

    Time: 68 ms (76.10%), Space: 43.6 MB (93.07%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.js
+++ 0735-asteroid-collision/0735-asteroid-collision.js
@@ -3,28 +3,21 @@
  * @return {number[]}
  */
 var asteroidCollision = function(asteroids) {
-    let stack = []
-    let i = 0
+    let stack = [], i = 0
+    
     while (i < asteroids.length) {
         if (stack.length === 0) {
             stack.push(asteroids[i])
             i++
-            // console.log('1', stack)
             continue
         }
         let top = stack[stack.length-1], topSign = Math.sign(top)
-        // console.log(top, 'top')
         if (topSign == 1 && asteroids[i] > 0 || topSign == -1 && (asteroids[i] < 0 || asteroids[i] > 0)) {
             stack.push(asteroids[i])
-            // console.log('2', stack)
         } else {
-        //handle if opposing signs
-            //   -5 (+) 10 === 5 which is greater than 0
             if (asteroids[i] + top < 0) {
-                // console.log('here', asteroids[i], top)
                 stack.pop(); 
                 i--
-                // stack.push(asteroids[i])
             } else if (asteroids[i] + top > 0 && top > 0) {
                 i++
                 continue
@@ -32,7 +25,6 @@ var asteroidCollision = function(asteroids) {
             else stack.pop()
         }
         i++
-        // console.log(stack)
     }
     return stack
 };
