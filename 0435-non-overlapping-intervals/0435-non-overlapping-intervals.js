3e77b4de11493bf373c7866f78653a258f96d021
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-07-24 10:03:00 -0400

    Time: 232 ms (95.18%), Space: 74.9 MB (61.57%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
@@ -3,21 +3,15 @@
  * @return {number}
  */
 var eraseOverlapIntervals = function(intervals) {
-    intervals.sort((a,b) => a[0] - b[0]).sort((a,b) => a[1] - b[1])
+    intervals.sort((a,b) => a[1] - b[1])
 
     let remove = 0
     let lastCompared = -Infinity
     for (let i = 0; i<intervals.length; i++) {
         const [x, y] = intervals[i]
-        console.log(x,y)
+        
         if (x >= lastCompared) lastCompared = y
         else remove++
-        // if (intervals[i][0] < intervals[lastCompared][1]) {
-        //     console.log(intervals[i], intervals[lastCompared], i)
-        //     lastCompared = i-1
-        //     console.log('last', lastCompared)
-        //     remove++
-        // } else lastCompared = i
     }
     return remove
 };
