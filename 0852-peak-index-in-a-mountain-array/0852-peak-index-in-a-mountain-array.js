9f8fb2b6508a4170585e3103fe29e1ec4735e80c
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-07-25 12:36:26 -0400

    Time: 78 ms (40.12%), Space: 50.9 MB (90.33%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
@@ -4,6 +4,6 @@
  */
 var peakIndexInMountainArray = function(arr) {
     for (let i=1; i<arr.length; i++) {
-        if (arr[i-1] < arr[i] && arr[i] > arr[i+1]) return i
+        if (arr[i] > arr[i+1]) return i
     }
 };
