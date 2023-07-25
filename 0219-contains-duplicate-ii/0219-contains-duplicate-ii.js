2d657d5959ac8feafa81301e1598d9569f20d8d9
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-07-25 15:03:38 -0400

    Time: 5357 ms (16.01%), Space: 50.8 MB (80.32%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -4,10 +4,10 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
-    for (let i=0; i < nums.length; i++) {
+    for (let i=0; i< nums.length; i++) {
         let j = nums.length-1
-        while (i<j) {
-            if (nums[i] == nums[j] && Math.abs(i-j) <=k) return true
+        while (i < j) {
+            if (nums[i] == nums[j] && Math.abs(i-j) <= k) return true
             j--
         }
     }
