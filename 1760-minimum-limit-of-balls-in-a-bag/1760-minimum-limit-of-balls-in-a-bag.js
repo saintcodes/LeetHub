765b6fbf906f51e972ee02a13db9883e02889e67
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-08-02 15:59:09 -0400

    Time: 109 ms (86.67%), Space: 56.1 MB (6.67%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.js
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.js
@@ -7,11 +7,15 @@ var minimumSize = function(nums, maxOperations) {
     let total = nums.reduce((c, a) => c+=a)
     let left = 1, right = Math.floor(total/maxOperations)
 
+    //1, 2, 3, 4, 5, 6, 7
+    //            l
+    //            m
+    //            r
     
     while (left < right) {
-        let mid = Math.floor((left+right)/2)
-        let sum = 0
+        let mid = Math.floor((left+right)/2), sum = 0
         for (let x of nums) sum+=Math.floor((x-1)/mid)
+        console.log('l:', left, 'r:', right, 'mid:', mid, 'sum:', sum)
         if (sum > maxOperations) left = mid+1
         else right = mid
     }
