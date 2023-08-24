37bd36e454370890ff042584d0358f415a8a7b6f
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-08-24 12:16:28 -0400

    Time: 252 ms (25.87%), Space: 53 MB (5.13%) - LeetHub

---
+++ 2815-max-pair-sum-in-an-array/2815-max-pair-sum-in-an-array.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSum = function(nums) {
+    let map = {}
+    for (let i=0; i<nums.length; i++) {
+        let maxDigit = Math.max(...nums[i].toString())
+        if (map[maxDigit]) map[maxDigit].push(nums[i])
+        else map[maxDigit] = [nums[i]]
+    }
+    // console.log(map)
+    let max = 0
+    for (let key in map) {
+        
+        if (map[key].length > 1) {
+            let arr = map[key].sort((a,b) => a-b)
+            console.log(arr)
+            let sum = arr[arr.length-1] + arr[arr.length-2]
+            if (sum > max) max = sum
+        }
+    }
+    return max || -1
+    
+}
+   
