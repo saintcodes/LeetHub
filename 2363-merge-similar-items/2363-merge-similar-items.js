c45d891a2645dbc4fa1b788bbfc0a2938f1bdddc
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-08-24 12:56:30 -0400

    Time: 88 ms (81.38%), Space: 50 MB (38.62%) - LeetHub

--- 2363-merge-similar-items/2363-merge-similar-items.js
+++ 2363-merge-similar-items/2363-merge-similar-items.js
@@ -6,16 +6,13 @@
 var mergeSimilarItems = function(items1, items2) {
     let ret = {}
     for (let i=0; i<items1.length; i++) {
-        // console.log(items1[i])
         if (ret[items1[i][0]]) ret[items1[i][0]].push(items1[i][1]) 
         else ret[items1[i][0]] = +items1[i][1]
     }
     for (let i=0; i<items2.length; i++) {
-        // console.log(items1[i])
         if (ret[items2[i][0]]) ret[items2[i][0]] = +ret[items2[i][0]] + items2[i][1]
         else ret[items2[i][0]] = +items2[i][1]
     }
-    console.log(ret)
     let arr = []
     for (let key in ret) {
         arr.push([+key, ret[key]])
