f72169f4c9d989a207fb39da271ca44111649023
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-09-14 14:42:00 -0400

    Time: 60 ms (42.94%), Space: 42.2 MB (86.61%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -6,12 +6,8 @@ var minCostClimbingStairs = function(cost) {
     let res = 0
     
     for (let i=2; i<cost.length; i++) {
-        // console.log(i, cost[i])
         cost[i] = cost[i] + Math.min(cost[i-2], cost[i-1])
-        // console.log('after', i, cost[i])
     }
-    // let first=cost.pop()
-    // let second=cost.pop()
-    // return Math.min(first, second)
+
     return Math.min(cost[cost.length-1], cost[cost.length-2])
 };
