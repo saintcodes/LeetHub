0759d75bf7237d4c63263e49199603e4a026cb21
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-09-14 12:25:25 -0400

    Time: 59 ms (97.09%), Space: 47.7 MB (80.72%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.js
+++ 0338-counting-bits/0338-counting-bits.js
@@ -2,17 +2,39 @@
  * @param {number} n
  * @return {number[]}
  */
-var countBits = function(n) {
-    let ans = []
+var countBits = function(num) {
+//     let ans = []
     
-    for (let i=0; i < n+1; i++) {
-        let binary = i.toString(2)        
-        let count = 0
+//     for (let i=0; i < n+1; i++) {
+//         let binary = i.toString(2)        
+//         let count = 0
         
-        for (digit of binary) {
-            if (digit == '1') count++
-        }
-        ans.push(count)
+//         for (digit of binary) {
+//             if (digit == '1') count++
+//         }
+//         ans.push(count)
+//     }
+//     return ans
+    let dp = new Array(num+1);
+    
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 1;
+    
+    if (num < 3) {
+        return dp.slice(0, num+1);
     }
-    return ans
+    
+    let c = 2;
+    
+    for (let i = 3; i <= num; i++) {
+        if (i == c*2) {
+            c = c*2;
+            dp[i] = 1;
+        } else {
+            dp[i] = dp[c] + dp[i-c];
+        }
+    }
+    
+    return dp;
 };
