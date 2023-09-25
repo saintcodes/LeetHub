ee95b519477d7a3c8238fd108eb0af4d05a5cbd4
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-09-25 14:26:10 -0400

    Time: 56 ms (68.74%), Space: 43.8 MB (65.86%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.js
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -8,14 +8,10 @@ var jump = function(nums) {
     while(currEnd < nums.length-1){
         let maxReach = 0;
         for(let i=currBeg; i<currEnd+1; i++){
-            console.log('nums[i]:', nums[i])
-            console.log(`i=${i}`, 'nums[i]+i:', nums[i]+i)
             maxReach = Math.max(maxReach, nums[i]+i);
-            console.log('maxReach:', maxReach)
         }
         currBeg = currEnd+1;
         currEnd = maxReach;
-        console.log(currBeg, currEnd)
         jumps++;
         if (currEnd > nums.length-1) return jumps
 
