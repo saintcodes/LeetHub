0e048e46fc38d6cf94878ae97ee1a47065058fcd
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-09-29 14:02:13 -0400

    Time: 71 ms (83.95%), Space: 53.3 MB (40.41%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.js
+++ 0896-monotonic-array/0896-monotonic-array.js
@@ -5,13 +5,10 @@
 var isMonotonic = function(nums) {
     let decrease=true, increase=true
 
-    // i-1, i
-    // [1,  2,  2,  3]
-    // inc =
-    // dec = false
     for (let i=1; i<nums.length; i++) {
         if (nums[i-1] < nums[i]) decrease = false
         if (nums[i-1] > nums[i]) increase = false
+        if (!decrease && !increase) return false
     }
     return decrease || increase
 };
