ebc8d6de3003fac2e5361c48a9b9c53838274632
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-09-29 16:42:41 -0400

    Time: 66 ms (6.13%), Space: 41.8 MB (78.32%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
@@ -3,16 +3,19 @@
  * @return {number}
  */
 var sumOfUnique = function(nums) {
-    let counter = {}
-    let sum = 0
-    nums.forEach((num) => {
-        counter[num] = (counter[num] || 0) +1
-    })
+//     let counter = {}
+//     let sum = 0
+//     nums.forEach((num) => {
+//         counter[num] = (counter[num] || 0) +1
+//     })
     
-    for (let key in counter) {
-        if (counter[key] == 1) sum+= +key
-    }
+//     for (let key in counter) {
+//         if (counter[key] == 1) sum+= +key
+//     }
     
-    return sum
+//     return sum
+    
+   return nums.filter((el, index, arr) => arr.indexOf(el) === arr.lastIndexOf(el)).reduce((a, b) => a + b, 0)
+
 };
 
