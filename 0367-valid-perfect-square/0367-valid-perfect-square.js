73d6c522f80c1c5e052cee72f6231930a72236f6
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-09-29 15:31:20 -0400

    Time: 53 ms (53.33%), Space: 42 MB (26.48%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.js
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -3,10 +3,16 @@
  * @return {boolean}
  */
 var isPerfectSquare = function(num) {
-    let square = Math.sqrt(num)
     
-    if (Number.isInteger(square)) {
-        return square*square === num
-    } else return false
+    let i = 1
+    while (i**2 <=num) {
+        if (i**2 == num) return true
+        i++
+    } return false
     
+//     let square = Math.sqrt(num)
+    
+//     if (Number.isInteger(square)) {
+//         return square*square === num
+//     } else return false
 };
