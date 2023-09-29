b1bee5363d46b25333eeaca79c651e1a2f06c700
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-09-29 16:09:34 -0400

    Time: 55 ms (76.22%), Space: 42.7 MB (25.61%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.js
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.js
@@ -3,18 +3,14 @@
  * @return {boolean}
  */
 var judgeSquareSum = function(c) {
-     var r = Math.floor(Math.sqrt(c));
-    var l = 0;
+    let r = Math.floor(Math.sqrt(c));
+    let l = 0;
      
-    while(l <= r){
-        var sum  = l**2 + r**2
-        if( sum === c){
-            return true
-        }else if(sum < c){
-            l++
-        }else{
-            r--
-        }
+    while (l <= r){
+        let sum = l**2 + r**2
+        if (sum === c) return true
+        else if(sum < c) l++
+        else r--
     }
     return false
 };
