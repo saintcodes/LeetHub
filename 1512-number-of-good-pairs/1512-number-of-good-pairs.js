0d0b33e078a56386519c1a5044981b0b23b6a668
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-10-03 12:26:28 -0400

    Time: 60 ms (11.52%), Space: 41.8 MB (52.31%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.js
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -4,15 +4,11 @@
  */
 var numIdenticalPairs = function(nums) {
     let count = 0
-    let j = nums.length-1
-    for (let i=0; i<nums.length-1; i++) {
+    for (let i=0, j = nums.length-1; i<nums.length-1; i++) {
         while (j > i) {
             if (nums[i] == nums[j] && i < j) {
-                count++
-                j--
-            } else {
-                j--
-            }
+                count++; j--;
+            } else j--
         }
         j = nums.length-1
     }
