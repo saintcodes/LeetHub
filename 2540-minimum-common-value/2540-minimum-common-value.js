d6ac723628a80f50efd1c225ef0587a1b6636e83
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-10-04 12:23:08 -0400

    Time: 79 ms (49.05%), Space: 49.8 MB (49.81%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.js
+++ 2540-minimum-common-value/2540-minimum-common-value.js
@@ -4,8 +4,14 @@
  * @return {number}
  */
 var getCommon = function(nums1, nums2) {
-    for (let i=0; i< nums1.length; i++) {
-        if (nums2.includes(nums1[i])) return nums1[i]
+    let i=0, j=0
+    while (i<nums1.length && j<nums2.length) {
+        if (nums1[i] == nums2[j]) return nums1[i]
+        else if (nums1[i] > nums2[j]) j++
+        else i++
     }
+    // for (let i=0; i< nums1.length; i++) {
+    //     if (nums2.includes(nums1[i])) return nums1[i]
+    // }
     return -1
 };
