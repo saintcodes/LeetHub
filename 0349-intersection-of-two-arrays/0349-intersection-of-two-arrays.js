8bee7808ecaaae39602419d59dfc94965a5e52d0
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-10-05 11:50:33 -0400

    Time: 47 ms (90.11%), Space: 43.3 MB (57.15%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -6,6 +6,6 @@
 var intersection = function(nums1, nums2) {
     let n1Set = new Set(nums1)
     let n2Set = new Set(nums2)
-    let res = [...n1Set].filter((n) => n2Set.has(n))
+    let res = [...n1Set].filter((n) => nums2.includes(n))
     return res
 };
