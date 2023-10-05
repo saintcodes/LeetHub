295090a2988bfb37cc233fed2474e36c204ce7cc
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-10-05 17:01:00 -0400

    Time: 63 ms (64.58%), Space: 44.7 MB (36.11%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.js
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.js
@@ -4,15 +4,13 @@
  * @return {number}
  */
 var minimumDifference = function(nums, k) {
-    // if (nums.length == k) return 0
     
     let diff = Infinity
     nums.sort((a,b) => a-b)
-    console.log(nums)
     for (let i=0; i<nums.length-(k-1); i++) {
-        console.log('hi', nums[i+(k-1)], nums[i])
-        diff = Math.min(nums[i+(k-1)]-nums[i], diff)
-        console.log('diff', diff)
+        let curr = nums[i+(k-1)]-nums[i]
+        diff = Math.min(curr, diff)
     }
+    
     return diff
 };
