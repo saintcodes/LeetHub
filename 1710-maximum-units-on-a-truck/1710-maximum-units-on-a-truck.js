8d9f4b068694dd29aab091e6de02835889b7ac26
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-10-06 14:23:08 -0400

    Time: 187 ms (5.05%), Space: 51.6 MB (5.05%) - LeetHub

---
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} boxTypes
+ * @param {number} truckSize
+ * @return {number}
+ */
+var maximumUnits = function(boxTypes, truckSize) {
+    boxTypes.sort((a,b) => b[1] - a[1])
+    console.log(boxTypes)
+    let sum=0
+
+    for (let i=0; i<boxTypes.length; i++) {
+        if (boxTypes[i][0] < truckSize) {
+            sum+=boxTypes[i][0]*boxTypes[i][1]
+            truckSize-=boxTypes[i][0]
+            console.log('hi', truckSize, sum)
+        } else if (boxTypes[i][0] >= truckSize) {
+            sum+=boxTypes[i][1]*truckSize
+            truckSize-=truckSize
+        }
+    }
+    return sum
+};
