892741a663184889ac96bed5a6f8f05f355c3164
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-10-11 19:25:14 -0400

    Time: 55 ms (41.28%), Space: 42.9 MB (18.10%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    
+// [5,7,7,8,8,10]
+//        l m  r
+    
+    let left = 0, right = nums.length-1
+    let start = -1, end = -1
+    while (left <= right) {
+        let mid = Math.floor(left + (right-left)/2)
+        if (nums[mid] === target) {
+            console.log(nums[mid])
+           start = end = mid
+            console.log(start, end)
+            while (start > 0 && nums[start-1] === target) start--
+            while (end < nums.length && nums[end+1] === target) end++
+            break
+        }
+        if (nums[mid] < target) left = mid+1
+        else right = mid-1
+    }
+    return [start, end]
+};
