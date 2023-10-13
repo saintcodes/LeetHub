24f44490e860f46509ae0a96bd6fda82243b3025
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-10-13 11:52:37 -0400

    Time: 50 ms (71.56%), Space: 42.2 MB (33.03%) - LeetHub

--- 1184-distance-between-bus-stops/1184-distance-between-bus-stops.js
+++ 1184-distance-between-bus-stops/1184-distance-between-bus-stops.js
@@ -5,12 +5,7 @@
  * @return {number}
  */
 var distanceBetweenBusStops = function(distance, start, destination) {
-    //  start = 7, end = 2
-    //  [1, 2, 3,4, 5, 6,7,8,9,10]
-    //  [14,13,4,7,10,17,8,3,2,13]
-    //  opt1 = 5+0+7+10 = 22
-    //  opt2 = 1+12+11+14 = 38
-    
+  
     let op1, op2
     if (destination < start) {
         op1 = distance.splice(destination, start-destination).reduce((acc,cum) => acc+=cum)
