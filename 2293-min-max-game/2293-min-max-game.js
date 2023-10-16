127ce162e3755fbda318806cdf1687a3524f76b2
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-10-16 11:31:43 -0400

    Time: 57 ms (45.98%), Space: 44.1 MB (27.59%) - LeetHub

--- 2293-min-max-game/2293-min-max-game.js
+++ 2293-min-max-game/2293-min-max-game.js
@@ -3,22 +3,18 @@
  * @return {number}
  */
 var minMaxGame = function(nums) {
-    if (nums.length == 2) return Math.min(nums[0], nums[1])
+    // if (nums.length == 2) return Math.min(nums[0], nums[1])
     
-    let temp = [], flip = true, count = (nums.length/2)-1
-    // console.log(count, flip)
+    let temp = [], flip = true
 
     while (nums.length >=2) {
-        // console.log(count, flip)
         for (let i=0; i< nums.length-1; i+=2) {
             if (flip) temp.push(Math.min(nums[i], nums[i+1]))
             else temp.push(Math.max(nums[i], nums[i+1]))
             flip = !flip
-            // console.log(temp, nums)
         }
         nums = temp
         temp = []
-        // count--
     }
     return nums[0]
 };
