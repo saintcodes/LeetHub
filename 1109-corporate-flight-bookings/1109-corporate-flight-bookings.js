a773b153d7dd6c274c173ae16d0e5be81ffaab33
saintcodes <87248728+saintcodes@users.noreply.github.com>
2023-11-04 16:18:21 -0400

    Time: 1645 ms (10.53%), Space: 62.1 MB (63.16%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} bookings
+ * @param {number} n
+ * @return {number[]}
+ */
+var corpFlightBookings = function(bookings, n) {
+    let arr = new Array(n).fill(0)
+    for (let i=0; i<bookings.length; i++) {
+        
+        let curr = bookings[i][0]-1
+        
+        while (curr < bookings[i][1]) {
+            arr[curr] += bookings[i][2]
+            curr++
+        }
+    }
+    return arr
+};
